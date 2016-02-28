7fc20cbaf202d4eade2a09876e3e8d88e0d72a9a
Smolyak Andrey <simpleondrew@ya.ru>
2016-02-28 12:46:35 +0600

    Add ability to remove items

--- app/src/main/java/org/oway_team/oway/NavigationItemsAdapter.java
+++ app/src/main/java/org/oway_team/oway/NavigationItemsAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.oway_team.oway.json.JSONNavigationItem;
@@ -21,6 +22,7 @@ public class NavigationItemsAdapter extends ArrayAdapter<JSONNavigationItem> {
     class ViewHolder {
         ImageView categoryImageView;
         TextView textTv;
+        LinearLayout ll;
     }
     public NavigationItemsAdapter(Context context, int resource, List<JSONNavigationItem> objects) {
         super(context, resource, objects);
@@ -32,7 +34,7 @@ public class NavigationItemsAdapter extends ArrayAdapter<JSONNavigationItem> {
         return mItems;
     }
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         View rowView = convertView;
         ViewHolder holder;
         if (rowView == null) {
@@ -42,10 +44,12 @@ public class NavigationItemsAdapter extends ArrayAdapter<JSONNavigationItem> {
             holder = new ViewHolder();
             holder.categoryImageView = (ImageView)rowView.findViewById(R.id.navigation_list_item_category_logo);
             holder.textTv = (TextView)rowView.findViewById(R.id.navigation_list_item_text);
+            holder.ll = (LinearLayout)rowView.findViewById(R.id.bottom_wrapper);
             rowView.setTag(holder);
         } else {
             holder = (ViewHolder)rowView.getTag();
         }
+
         String type = mItems.get(position).type;
         Log.d("WOW", "Inflating item: " + type);
         if (type.equalsIgnoreCase("company")) {
@@ -57,6 +61,12 @@ public class NavigationItemsAdapter extends ArrayAdapter<JSONNavigationItem> {
         }
 
         holder.textTv.setText(mItems.get(position).title);
+        holder.ll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                remove(mItems.get(position));
+            }
+        });
 
         return rowView;
     }
